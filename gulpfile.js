f1fc7014c210c29fbb74c8236869823fea787227
Santiago Rojo <tiagox@gmail.com>
2015-10-15 16:24:41 -0300

    Add compile task for SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
   browserSync = require('browser-sync'),
-  reload = browserSync.reload;
+  reload = browserSync.reload,
+  sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('css/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css/build'));
+});
 
 gulp.task('serve', ['sass'], function() {
   browserSync({
@@ -10,4 +17,5 @@ gulp.task('serve', ['sass'], function() {
   });
 
   gulp.watch(['*.html', 'css/build/**/*.css', 'js/**/*.js'], {cwd: '.'}, reload);
+  gulp.watch(['css/sass/**/*.scss'], {cwd: '.'}, ['sass']);
 });
