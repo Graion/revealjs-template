882a5c32b72b07a5cd94753122b369a295d16e11
Santiago Rojo <tiagox@gmail.com>
2016-01-18 20:14:03 -0300

    Add build recipe for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
   browserSync = require('browser-sync'),
   reload = browserSync.reload,
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  del = require('del');
 
 gulp.task('sass', function () {
   gulp.src('css/sass/**/*.scss')
@@ -19,3 +20,25 @@ gulp.task('serve', ['sass'], function() {
   gulp.watch(['*.html', 'css/build/**/*.css', 'js/**/*.js'], {cwd: '.'}, reload);
   gulp.watch(['css/sass/**/*.scss'], {cwd: '.'}, ['sass']);
 });
+
+gulp.task('clean', function () {
+  return del('dist/**/*');
+});
+
+gulp.task('build', ['clean'], function () {
+  gulp.src(['index.html'])
+    .pipe(gulp.dest('dist'));
+
+  gulp.src(['js/**/*.js'])
+    .pipe(gulp.dest('dist/js'));
+
+  gulp.src(['css/**/*.css'])
+    .pipe(gulp.dest('dist/css'));
+
+  gulp.src(['components/reveal.js/lib/**/*.{css,js,eot,ttf,woff}'])
+    .pipe(gulp.dest('dist/lib'));
+
+  gulp.src(['components/reveal.js/**/*.{css,js,eot,ttf,woff}'])
+    .pipe(gulp.dest('dist/components/reveal.js'));
+});
+
